9a273e25a8366bb5149454a6e8d28a963fddf68b
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2020-05-07 14:13:53 +0300

    Refactoring

--- scripts/app.js
+++ scripts/app.js
@@ -10,8 +10,8 @@ document.addEventListener("mousemove", function (e) { MoveBackground(e); });
 
 function MoveBackground(e)
 {
-	let offsetX = (e.clientX / window.innerWidth * 50) - 25;
-	let offsetY = (e.clientY / window.innerHeight * 50) - 25;
+	let offsetX = (e.clientX / window.innerWidth * 30) - 15;
+	let offsetY = (e.clientY / window.innerHeight * 10) - 5;
 
-	bgTree.setAttribute("style", "left: " + offsetX + "px; top: " + offsetY + "px;");
+	bgTree.setAttribute("style", "background-position: " + offsetX + "px " + offsetY + "px;");
 }
