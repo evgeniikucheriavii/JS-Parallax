013d694712bc9c8c03b896c54137af27dce20df7
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2020-05-07 21:45:22 +0300

    Added comments

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,18 @@
+//Получаем элемент фона с деревом
 const bgTree = document.getElementById("background-tree");
 
-const parallax =
-{
-	x: 0,
-	y: 0
-};
-
+//При движении мышью вызываем функцию, которая меняет положение фона
 document.addEventListener("mousemove", function (e) { MoveBackground(e); });
 
 function MoveBackground(e)
 {
+	//Высчитываем, насколько далеко от начала оси находится курсор: 0 - 0, 1 - ширина экрана
+	//Затем умножаем получившееся число на 30 - настолько будет сдвигаться фон
+	//Например, если курсор находится посередине страницы (0.5), то при умножении получится 15
+	//Далее отнимаем половину от 30, чтобы фон мог двигаться как влево, так и вправо
 	let offsetX = (e.clientX / window.innerWidth * 30) - 15;
 	let offsetY = (e.clientY / window.innerHeight * 10) - 5;
 
+	//Меняем положение фона
 	bgTree.setAttribute("style", "background-position: " + offsetX + "px " + offsetY + "px;");
 }
--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,30 +1,36 @@
+//Создаём класс для частиц
 class Particle
 {
+	//Конструктор принимает положение частицы по трём осям и цвет
 	constructor(x, y, z, color)
 	{
 		this.x = x;
 		this.y = y;
 		this.z = z;
 
+		//Размытие и скорость зависят от положения частицы по оси Z
+		//Чем выше частица, тем более размытой она будет и тем быстрее она будет двигаться 
 		let blurs = [ 0, 2, 1, 0 ];
 
 		this.blur = blurs[z];
 		this.speed = z;
 		this.color = color;
-
-		this.elem = null;
 	}
 
+	//Метод движения частицы
 	Move(d)
 	{
 		this.y += this.speed * d;
 	}
 }
 
+//Позиция полосы прокрутки
 let scrollPosition = 0;
 
+//Получаем контейнер для частиц
 const particlesContainer = document.getElementById("particles");
 
+//Создаём массив с частицами
 const particles = 
 [
 	new Particle(1650, 450, 3, "#FF7019"),
@@ -35,13 +41,17 @@ const particles =
 	new Particle(1200, 900, 2, "#FF7019"),
 ];
 
+//Эта функция вывод частицы на страницу
 Fill();
 
+//При каждой прокрутке вызываем функцию Scroll(), которая двигает частицы
 window.addEventListener("scroll", function (e) { Scroll(e); });
 
 function Scroll(e)
 {
+	//Определяем, в каком направлении была прокрутка
 	let d = 0;
+
 	if(window.pageYOffset > scrollPosition)
 	{
 		d = 1;
@@ -53,18 +63,22 @@ function Scroll(e)
 	
 	scrollPosition = window.pageYOffset;
 
+	//Двигаем все частицы в заданном направлении
 	for(let i = 0; i < particles.length; i++)
 	{
 		particles[i].Move(d);
 	}
 
+	//Выводим всё на страницу
 	Fill();
 }
 
 function Fill()
 {
+	//Очищаем контейнер
 	particlesContainer.innerHTML = "";
 
+	//Создаём новые элементы с обновлёнными свойствами и помещаем их в контейнер
 	for(let i = 0; i < particles.length; i++)
 	{
 		let div = document.createElement("div");
